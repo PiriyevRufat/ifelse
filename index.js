be655d468b965a1e38910bf77c8d870b3466b481
PiriyevRufat <tu7pj36at@code.edu.az>
2024-04-03 00:26:35 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// let x = parseInt(prompt("birinci ededi elave edin"));
+// let y = parseInt(prompt("ikinci ededi elave edin"));
+// let z = parseInt(prompt("ucunci ededi elave edin"));
+
+// let mediumQuery =(x+y+z)/3
+// alert(mediumQuery);
+
+// Task2
+
+// let x = parseInt(prompt("birinci ededi elave edin"));
+// let y = parseInt(prompt("ikinci ededi elave edin"));
+// let z = parseInt(prompt("ucunci ededi elave edin"));
+
+// if (x+z>y && x+y>z && y+z>x){
+//     alert("Ucbucag ola biler");
+// }
+// else{
+//    alert("Ucbucag ola bilmez");
+// }
+
+// Task3
+
+// let surName=prompt("Soyadivi yaz");
+// let name=prompt("Adivi yaz");
+
+// let fullName=`${surName} ${name}`
+// alert(fullName);
+
+// Task4
+
+// let height=parseFloat(prompt("Boyuvuzu yere soxum"));
+// let weight=parseInt(prompt("Cekivizi qeyd edin"));
+
+// let bodyMassIndex=weight/(height**2);
+
+// alert(bodyMassIndex);
